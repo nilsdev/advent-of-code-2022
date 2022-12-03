2374bbd36f49bceb59a802d478347dabbba01bbe
nilsdev <nilsdev+1@protonmail.com>
2022-12-03 18:41:29 +0100

    changed comment formating

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,38 +1,54 @@
 use std::fs; 
 fn main() {
-    let input_content = fs::read_to_string("./input")   // reading input file to input_content : String
+    // reading input file to input_content : String
+    let input_content = fs::read_to_string("./input") 
         .expect("Error reading input file"); 
 
-    let mut calories : Vec<i32> = Vec::new();            // vector to store the elf / caloties list in 
+    // vector to store the elf / caloties list in 
+    let mut calories : Vec<i32> = Vec::new();            
 
-    calories.push(0);                                    // create first elf
+    // create first elf
+    calories.push(0);                                    
 
-    for line in input_content.lines()  {                // for line in input_content String
-        if line.is_empty() {                            // if line is empty 
-            calories.push(0);                            // create new elf / push new element on vector
-        } else {                                        // else 
-            *calories.last_mut().unwrap()                // return mutable pointer to last element of list and derefrence it
-                += line.parse::<i32>().unwrap();        // add the new number on top 
+    // for line in input_content String
+    for line in input_content.lines()  {                
+        if line.is_empty() {                            
+            // create new elf / push new element on vector
+            calories.push(0);                           
+        } else {                                        
+            // return mutable pointer to last element of list and derefrence it
+            *calories.last_mut().unwrap()                
+                // add the new number on top 
+                += line.parse::<i32>().unwrap();        
         }
     }
 
+    // print highest number in vector
     println!("Part 1: {}", 
         calories.iter().max().unwrap()
-    );                                                   // print highest number in vector
+    );                                                   
 
-    let mut top_three : Vec<i32> = Vec::new();          // vector to store the top 3 elfes in
+    // vector to store the top 3 elfes in
+    let mut top_three : Vec<i32> = Vec::new();          
 
     for _ in 0..3 {
-        let max = *calories.iter().max().unwrap();       // get current max value
-        top_three.push(max);                            // push max value on top_three vector
-        calories.swap_remove(                            // find next largest elf by: // using swap_remove() to avoid O(n) performance of remove()
-            calories.iter()                              // iterating over calories Vector
-            .position(|&x| x == max)                    // find index of current max value 
-            .unwrap()                                   // and removing current max elf
-        );  
+        // get current max value
+        let max = *calories.iter().max().unwrap();       
+        // push max value on top_three vector
+        top_three.push(max);                            
+        // find next largest elf by: 
+        calories.swap_remove(                           
+            // iterating over calories Vector
+            calories.iter()                             
+            // find index of current max value 
+            .position(|&x| x == max)                    
+            .unwrap()                                   
+        // and removing current max elf 
+        ); 
     }
 
+    // print highest number in vector
     println!("Part 2: {:?}", 
         top_three.iter().sum::<i32>()
-    );                                               // print highest number in vector
+    );                                                  
 }
