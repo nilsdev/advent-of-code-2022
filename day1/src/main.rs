790d1207350ccc91fe702503fed987d99485213c
nilsdev <nilsdev+1@protonmail.com>
2022-12-03 18:36:40 +0100

    calories

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,30 +3,30 @@ fn main() {
     let input_content = fs::read_to_string("./input")   // reading input file to input_content : String
         .expect("Error reading input file"); 
 
-    let mut cal_vec : Vec<i32> = Vec::new();            // vector to store the elf / caloties list in 
+    let mut calories : Vec<i32> = Vec::new();            // vector to store the elf / caloties list in 
 
-    cal_vec.push(0);                                    // create first elf
+    calories.push(0);                                    // create first elf
 
     for line in input_content.lines()  {                // for line in input_content String
         if line.is_empty() {                            // if line is empty 
-            cal_vec.push(0);                            // create new elf / push new element on vector
+            calories.push(0);                            // create new elf / push new element on vector
         } else {                                        // else 
-            *cal_vec.last_mut().unwrap()                // return mutable pointer to last element of list and derefrence it
+            *calories.last_mut().unwrap()                // return mutable pointer to last element of list and derefrence it
                 += line.parse::<i32>().unwrap();        // add the new number on top 
         }
     }
 
     println!("Part 1: {}", 
-        cal_vec.iter().max().unwrap()
+        calories.iter().max().unwrap()
     );                                                   // print highest number in vector
 
     let mut top_three : Vec<i32> = Vec::new();          // vector to store the top 3 elfes in
 
     for _ in 0..3 {
-        let max = *cal_vec.iter().max().unwrap();       // get current max value
+        let max = *calories.iter().max().unwrap();       // get current max value
         top_three.push(max);                            // push max value on top_three vector
-        cal_vec.swap_remove(                            // find next largest elf by: // using swap_remove() to avoid O(n) performance of remove()
-            cal_vec.iter()                              // iterating over cal_vec Vector
+        calories.swap_remove(                            // find next largest elf by: // using swap_remove() to avoid O(n) performance of remove()
+            calories.iter()                              // iterating over calories Vector
             .position(|&x| x == max)                    // find index of current max value 
             .unwrap()                                   // and removing current max elf
         );  
