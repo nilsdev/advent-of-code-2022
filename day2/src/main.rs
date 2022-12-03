a047a4d3e64c02bd575fe2ca6b25117e4b16148f
nilsdev <nilsdev+1@protonmail.com>
2022-12-03 20:52:41 +0100

    day2 without comments

---
+++ day2/src/main.rs
@@ -0,0 +1,47 @@
+use std::fs; 
+
+fn main() {
+    println!("Hello, world!");
+    let input_content = fs::read_to_string("./input")
+        .expect("Error reading input file"); 
+
+        let mut part1 : i32 = 0; 
+        let mut part2 : i32 = 0; 
+
+        for line in input_content.lines() {
+            let moves = line.split(" "); 
+            let vec = moves.collect::<Vec<&str>>(); 
+
+            match vec.as_slice() {
+                ["A", "X"] => part1 += 1 + 3,
+                ["A", "Y"] => part1 += 2 + 6, 
+                ["A", "Z"] => part1 += 3 + 0,
+
+                ["B", "X"] => part1 += 1 + 0,
+                ["B", "Y"] => part1 += 2 + 3,
+                ["B", "Z"] => part1 += 3 + 6,
+
+                ["C", "X"] => part1 += 1 + 6,
+                ["C", "Y"] => part1 += 2 + 0,
+                ["C", "Z"] => part1 += 3 + 3,
+                _ => todo!() 
+            }
+
+            match vec.as_slice() {
+                ["A", "X"] => part2 += 3 + 0,
+                ["A", "Y"] => part2 += 1 + 3, 
+                ["A", "Z"] => part2 += 2 + 6,
+
+                ["B", "X"] => part2 += 1 + 0,
+                ["B", "Y"] => part2 += 2 + 3,
+                ["B", "Z"] => part2 += 3 + 6,
+
+                ["C", "X"] => part2 += 2 + 0,
+                ["C", "Y"] => part2 += 3 + 3,
+                ["C", "Z"] => part2 += 1 + 6,
+                _ => todo!() 
+            }
+        }
+        println!("Part1: {}", part1); 
+        println!("Part2: {}", part2); 
+}
