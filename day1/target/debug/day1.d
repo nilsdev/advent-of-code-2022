c03c5f5af6f7146f86180f3e03fe245eb8c1c139
nilsdev <nilsdev+1@protonmail.com>
2022-12-03 17:34:31 +0100

    fix

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/nils/Documents/advent-of-code-2022/day1/target/debug/day1: /home/nils/Documents/advent-of-code-2022/day1/src/main.rs
--- day1/target/debug/deps/day1-a14c745ecb5028b1.d
+++
@@ -1,7 +0,0 @@
-/home/nils/Documents/advent-of-code-2022/day1/target/debug/deps/day1-a14c745ecb5028b1.rmeta: src/main.rs
-
-/home/nils/Documents/advent-of-code-2022/day1/target/debug/deps/day1-a14c745ecb5028b1.d: src/main.rs
-
-src/main.rs:
-
-# env-dep:CLIPPY_ARGS=
