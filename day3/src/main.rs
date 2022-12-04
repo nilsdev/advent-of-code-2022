ff425a170f55d67b8a4167f803f300df2326a128
nilsdev <nilsdev+1@protonmail.com>
2022-12-04 14:58:57 +0100

    day 3 using function

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -3,13 +3,18 @@ fn main() {
     let alphabet: Vec<char> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().collect();
     let input_content = fs::read_to_string("./input").expect("Error reading input file");
 
+    part1(input_content, alphabet); 
+
+}
+
+fn part1(input_content : String, alphabet : Vec<char>) {
     let mut result : usize = 0; 
     for line in input_content.lines() {
         let split = line.split_at(line.chars().count() / 2); 
         for letter in split.0.chars() {
             if split.1.contains(letter) {
                 let value = alphabet.iter().position(|&x| x == letter).unwrap() + 1; 
-                println!("Found: {} + {}", letter, value); 
+                // println!("Found: {} + {}", letter, value); 
                 result += value; 
                 break;
             }
